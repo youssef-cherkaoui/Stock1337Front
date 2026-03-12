d56f2eeb9c87b35138d55f779f6cf95f76525a8d
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-12 17:56:28 +0000

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './features/auth/login/login';
+import {RegisterComponent} from './features/auth/register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  //{path: 'home', component: HomeComponent},
+  {path : '', redirectTo : 'login', pathMatch: 'full'},
+];
