15cb62e9a0ba4380164b343414190bd65ce8bf97
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-12 17:58:47 +0000

    initial component

---
+++ src/app/directives/scroll-animation.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollAnimation } from './scroll-animation';
+
+describe('ScrollAnimation', () => {
+  it('should create an instance', () => {
+    const directive = new ScrollAnimation();
+    expect(directive).toBeTruthy();
+  });
+});
