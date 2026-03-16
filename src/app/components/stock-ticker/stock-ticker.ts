31ad4f1f0aa4eac8b7645fe2753584d755580666
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-16 17:55:38 +0000

    Stock-TICKER Comp

--- src/app/components/stock-ticker/stock-ticker.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-stock-ticker',
-  imports: [],
-  templateUrl: './stock-ticker.html',
-  styleUrl: './stock-ticker.css',
-})
-export class StockTicker {}
