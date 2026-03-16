685a803a58d52dbce15267ca829378ed57b9800a
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2026-03-16 18:30:49 +0000

    annimation

--- src/app/directives/scroll-animation.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollAnimation } from './scroll-animation';
-
-describe('ScrollAnimation', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollAnimation();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/scroll-animation.ts
+++
@@ -1,8 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[appScrollAnimation]',
-})
-export class ScrollAnimation {
-  constructor() {}
-}
